bab08e00b8f61d1b646b33c3097390d94a4785f8
Lizzie <lchan217@gmail.com>
2019-07-15 19:03:25 -0500

    add form for user's name

--- src/components/users/UserForm.js
+++ src/components/users/UserForm.js
@@ -1,1 +1,36 @@
-//persist to backend 
+//persist to backend
+import React from 'react';
+
+class UserForm extends React.Component {
+  state = {
+    name: '',
+  }
+
+  handleChange = event => {
+    this.setState({
+      value: event.target.value,
+    });
+  }
+
+    handleSubmit = event => {
+    event.preventDefault()
+    //this.sendFormDataSomewhere(this.state)
+  }
+
+  render() {
+    return (
+      <div>
+      Please enter your name: 
+        <form onSubmit={event => this.handleSubmit(event)}>
+          <input
+            type="text"
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default UserForm;
