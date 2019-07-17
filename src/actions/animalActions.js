afbfa391fd39a3c3c0f33829810228cf5f3be3b4
Lizzie <lchan217@gmail.com>
2019-07-17 08:57:30 -0500

    add actions for switching status

--- src/actions/animalActions.js
+++ src/actions/animalActions.js
@@ -6,3 +6,19 @@ export function fetchAnimals() {
       .then(animals => dispatch({ type: 'FETCH_ANIMALS', payload: animals}));
   };
 }
+
+export const deadToBreeding = () =>{
+  return {type: "DEAD_TO_BREEDING"}
+}
+
+export const breedingToWild = () =>{
+  return {type: "BREEDING_TO_WILD"}
+}
+
+export const endangeredToBreeding = () =>{
+  return {type: "ENDANGERED_TO_BREEDING"}
+}
+
+export const wildToDead = () =>{
+  return {type: "WILD_TO_DEAD"}
+}
