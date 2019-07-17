d88fc4857dad29998f839e7e721b5dcaa4562b83
Lizzie <lchan217@gmail.com>
2019-07-17 14:33:14 -0500

    add endangered to breeding reducer

--- src/reducers/animalsReducer.js
+++ src/reducers/animalsReducer.js
@@ -6,20 +6,26 @@ export default function animalsReducer(state = [], action) {
     case 'FETCH_ANIMALS':
     return action.payload
 
-    case 'ENDANGERED_TO_BREEDING':
-    debugger
-    return state
-
     case 'DEAD_TO_BREEDING':
     return state
 
     case 'BREEDING_TO_WILD':
     return state
 
+    case 'ENDANGERED_TO_BREEDING':
+    const dToB = [...state]
+    const result = dToB.map(animal => {
+      if(animal.id === action.id){
+        animal.status = "breeding"
+      }
+      return animal
+    })
+    return result
+
     case 'WILD_TO_DEAD':
     return state
 
     default:
-    return state;
+      return state;
   }
 }
