0eb2c9d920936391e1b3910d74dc6ddd8f5119b1
Lizzie <lchan217@gmail.com>
2019-07-17 18:15:54 -0500

    add instructions

--- src/components/welcome/Instructions.js
+++ src/components/welcome/Instructions.js
@@ -4,7 +4,10 @@ import React from 'react';
 const Instructions = () => {
   return (
     <div className="navbar">
-      Instructions
+      <h2>Save the animals!</h2>
+      <p>You have 15 animals who are at risk of becoming extinct. Capture
+      the animals and nurse them back to health. Once they have 5 health points,
+      they are ready to be released back into the wild. Good luck!</p>
     </div>
   );
 };
