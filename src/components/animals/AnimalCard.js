d87f72a2106a2c1fa3b66536a5b9e2ebef543a10
Lizzie <lchan217@gmail.com>
2019-07-18 17:38:49 -0500

    add name, img, health, and status to animalcard

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -6,6 +6,12 @@ const AnimalCard = ({animal}) =>
     <div class="image">
       <img src={animal.img} alt={`${animal.name}'s picture`} />
    </div>
+     <div class="content">
+       <div class="header">{animal.name}</div>
+        <div class="description">
+          Health: {animal.health} <br/> Status: {animal.status}
+        </div>
+     </div>
   </div>
 </div>
 
