4ca88c0784fb90667572e0a443f2b680956778b1
Lizzie <lchan217@gmail.com>
2019-07-18 13:25:49 -0500

    change classname of animal card

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
  export default class AnimalCard extends Component {
      render() {
         return (
-            <div className="endangeredList">
+            <div className="AnimalCard">
               {this.props.showEndangeredList}
               {this.props.showBreedingList}
               {this.props.showWildList}
