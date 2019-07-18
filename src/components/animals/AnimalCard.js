e0e891261ee66f8b628631851ccdbd37afbe01e6
Lizzie <lchan217@gmail.com>
2019-07-18 14:23:04 -0500

    delete mapDispatchToProps from animalcard

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -1,26 +1,13 @@
 
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import {breedingToWild, endangeredToBreeding, nurse} from '../../actions/animalActions.js'
 
-class AnimalCard extends Component {
+
+ export default class AnimalCard extends Component {
      render() {
         return (
             <div className="AnimalCard">
-              {this.props.showEndangeredList}
-              {this.props.showBreedingList}
-              {this.props.showWildList}
+
             </div>
         )
     }
 }
-
-const mapDispatchToProps = dispatch => {
-  return {
-    breedingToWild: (id) => dispatch(breedingToWild(id)),
-    endangeredToBreeding: (id) => dispatch(endangeredToBreeding(id)),
-    nurse: (id) => dispatch(nurse(id)),
-  }
-}
-
-export default connect(null, mapDispatchToProps)(AnimalCard)
