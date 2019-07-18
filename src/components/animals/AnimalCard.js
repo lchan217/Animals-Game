6e794f0dc7df98ecba6d22e5f38e0bc3fd7c7248
Lizzie <lchan217@gmail.com>
2019-07-18 17:52:38 -0500

    add buttons to card (no functionality yet)

--- src/components/animals/AnimalCard.js
+++ src/components/animals/AnimalCard.js
@@ -11,6 +11,11 @@ const AnimalCard = ({animal}) =>
         <div class="description">
           Health: {animal.health} <br/> Status: {animal.status}
         </div>
+        <div class="ui three buttons">
+          <div class="ui button">Capture</div>
+          <div class="ui button">Nurse</div>
+          <div class="ui button">Release</div>
+        </div>
      </div>
   </div>
 </div>
