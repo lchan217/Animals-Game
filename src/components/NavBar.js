07d634a12c577fbf1dc6ffc96081ddc30a817240
Lizzie <lchan217@gmail.com>
2019-07-20 10:14:07 -0500

    change order of linkl and add new game

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,10 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className='ui tabular menu'>
-      <div className="item"><Link to='/animals/instructions' exact>Instructions</Link></div>
-      <div className="item"><Link to='/users/highscores' exact>High Scores</Link></div>
+      <div className="item"><Link to='/' exact>New Game</Link></div>
       <div className="item"><Link to='/users/cards' exact>User Cards</Link></div>
+      <div className="item"><Link to='/users/highscores' exact>High Scores</Link></div>
+      <div className="item"><Link to='/animals/instructions' exact>Instructions</Link></div>
       <div className="item"><Link to='/animals/all' exact>All Animals</Link></div>
       <div className="item"><Link to='/animals/breeding' exact>Breeding Animals</Link></div>
       <div className="item"><Link to='/animals/endangered' exact>Endangered Animals</Link></div>
