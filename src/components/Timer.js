8379f0007d7c12e4dac2f4fdc00e6796d1ec3ed1
Lizzie <lchan217@gmail.com>
2019-07-20 11:14:54 -0500

    add timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -4,18 +4,37 @@ class Timer extends React.Component {
   constructor() {
   super();
   this.state = {
-    timer: 0
+    time: 0
   };
 }
 
+// componentDidMount() {
+// needs to happen onClick of first animal to be saved
+//   this.interval = setInterval(this.startClock, 1000)
+// }
+//
+// componentWillUnmount(){
+// needs to happen onCLick of last animal to be saved 
+//   clearInterval(this.interval);
+// }
 
   render() {
     return (
       <div>
-        Timer:
+          Timer: {this.state.time}
       </div>
     );
   }
+
+  // startClock = () => {
+  //   this.setState(prevState => ({
+  //     time: prevState.time+1
+  //   }))
+  // }
+  //
+  // stopClock = () => {
+  //   clearInterval(this.interval)
+  // }
 }
 
 export default Timer
