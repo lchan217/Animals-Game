4328ba40b7f0930483f965bd7aea14cc0abdc7f9
Lizzie <lchan217@gmail.com>
2019-07-19 19:21:48 -0500

    add usercard functionality

--- src/components/welcome/UserCard.js
+++ src/components/welcome/UserCard.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Card } from 'semantic-ui-react'
 
 const UserCard = ({name, age, occupation, score}) => {
 return (
   <div className="userCard">
-    
+    <div class="ui cards">
+      <Card>
+        <div class="content">
+          <div class="header">
+            {name}
+          </div>
+          <div class="meta">
+            Age: {age}
+          </div>
+          <div class="meta">
+            Occupation: {occupation}
+          </div>
+          <div class="description">
+            <b>Score: {score}</b>
+          </div>
+        </div>
+      </Card>
+    </div>
   </div>
   )
 }
