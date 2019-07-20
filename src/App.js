590367c5caa5fdebd6dd5dd1b70e229462dd41ab
Lizzie <lchan217@gmail.com>
2019-07-20 14:02:07 -0500

    move instructions out from being nested in animals

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import UserContainer from './containers/UserContainer.js'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import UserForm from './components/welcome/UserForm';
 import NavBar from './components/NavBar';
+import Instructions from './components/Instructions';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <div>
         <NavBar />
         <Route exact path='/'  component={UserForm} />
+        <Route exact path='/instructions'  component={Instructions} />
         <Route exact path='/users/:id' render= {({match}) => < UserContainer filter={match.params.id}/>} />
         <Route exact path='/animals/:id' render= {({match}) => < AnimalContainer filter={match.params.id}/>} />
       </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,7 +7,7 @@ const NavBar = () => {
       <div className="item"><Link to='/' exact>New Game</Link></div>
       <div className="item"><Link to='/users/cards' exact>User Cards</Link></div>
       <div className="item"><Link to='/users/highscores' exact>High Scores</Link></div>
-      <div className="item"><Link to='/animals/instructions' exact>Instructions</Link></div>
+      <div className="item"><Link to='/instructions' exact>Instructions</Link></div>
       <div className="item"><Link to='/animals/all' exact>All Animals</Link></div>
       <div className="item"><Link to='/animals/breeding' exact>Breeding Animals</Link></div>
       <div className="item"><Link to='/animals/endangered' exact>Endangered Animals</Link></div>
