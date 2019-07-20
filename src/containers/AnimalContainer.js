eeddac81227666ab0207870f8ce6badc0ebd6a47
Lizzie <lchan217@gmail.com>
2019-07-20 16:42:14 -0500

    fix - cards arent random on every click

--- src/containers/AnimalContainer.js
+++ src/containers/AnimalContainer.js
@@ -12,41 +12,44 @@ class AnimalContainer extends Component {
      }
 // =================================================showlists=================================================================
      showAnimalCard(){
-       if (this.props.animals.length > 0  && this.props.filter === "breeding"){
-         let breeding =  this.props.animals.filter(animal => animal.status === "breeding")
-         return (
-           <Card.Group itemsPerRow={5}>
-             {breeding.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
-           </Card.Group>
-         )
-       }
-       if (this.props.animals.length > 0 && this.props.filter === "all"){
-         let all =  this.props.animals
+       // if (this.props.animals.length > 0  && this.props.filter === "breeding"){
+       //   let breeding =  this.props.animals.filter(animal => animal.status === "breeding")
+       //   return (
+       //     <Card.Group itemsPerRow={5}>
+       //       {breeding.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
+       //     </Card.Group>
+       //   )
+       // }
+
+       if (this.props.animals.length > 0){
+         const all =  this.props.animals.sort(function(a,b){
+           return a.id - b.id
+         })
          return (
            <Card.Group itemsPerRow={5}>
              {all.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
            </Card.Group>
          )
        }
-       if (this.props.animals.length > 0 && this.props.filter === "endangered"){
-         let endangered = this.props.animals.filter(animal => animal.status === "endangered")
-         return (
-           <Card.Group itemsPerRow={5}>
-             {endangered.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
-           </Card.Group>
-         )
-       }
-        if (this.props.animals.length > 0  && this.props.filter === "wild"){
-          let wild =  this.props.animals.filter(animal => animal.status === "wild")
-          return (
-            <Card.Group itemsPerRow={5}>
-              {wild.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
-            </Card.Group>
-          )
-        }
+       // if (this.props.animals.length > 0 && this.props.filter === "endangered"){
+       //   let endangered = this.props.animals.filter(animal => animal.status === "endangered")
+       //   return (
+       //     <Card.Group itemsPerRow={5}>
+       //       {endangered.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
+       //     </Card.Group>
+       //   )
+       // }
+        // if (this.props.animals.length > 0  && this.props.filter === "wild"){
+        //   let wild =  this.props.animals.filter(animal => animal.status === "wild")
+        //   return (
+        //     <Card.Group itemsPerRow={5}>
+        //       {wild.map((animal, index) => <AnimalCard key={index} {...animal}/>)}
+        //     </Card.Group>
+        //   )
+        // }
      }
 
-  threeRandom(){
+  threeRandom = () =>{
     if (this.props.animals.length > 0){
       const shuffled = this.props.animals.sort(() => 0.5 - Math.random())
       let selected = shuffled.slice(0, 3)
