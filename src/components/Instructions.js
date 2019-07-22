7b4e9e07b51f2ab8f96b90d76170316159727782
Lizzie <lchan217@gmail.com>
2019-07-22 09:01:35 -0500

    change instructions wording (minor)

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,9 +5,10 @@ const Instructions = (props) => {
   return (
     <div className="instructions">
       <h1>Save the animals!</h1>
-      <p>There are 15 animals who are at risk of becoming extinct. Unfortunately, only
-        three can be saved per session. Capture today's animals and nurse them back to health.
-        Once they have 5 health points,they are ready to be released back into the wild. Good luck!
+      <p>There are 15 animals who are at risk of becoming extinct. Unfortunately, only <br></br>
+        three can be saved per session. Capture today's animals and nurse them back to health.<br></br>
+        Once they have 5 health points,they are ready to be released back into the wild. Your timer<br></br>
+        will start when you navigate "All Animals." Good luck!
         </p>
     </div>
   );
