3c0f32450f02c8a490d2e735227803902755bdcd
Lizzie <lchan217@gmail.com>
2019-07-22 10:06:04 -0500

    Edit instructions (minor changes)

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -7,8 +7,9 @@ const Instructions = (props) => {
       <h1>Save the animals!</h1>
       <p>There are 15 animals who are at risk of becoming extinct. Unfortunately, only <br></br>
         three can be saved per session. Capture today's animals and nurse them back to health.<br></br>
-        Once they have 5 health points,they are ready to be released back into the wild. Your timer<br></br>
-        will start when you navigate "All Animals." Good luck!
+        Once they have 5 health points, they are ready to be released back into the wild. Your timer<br></br>
+      will start when you navigate to "All Animals." Don't forget to record your score afterwards and <br></br>
+        good luck!
         </p>
     </div>
   );
