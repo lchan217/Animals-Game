aa6c59083837582499f79e072496268a0118034d
Lizzie <lchan217@gmail.com>
2019-07-22 10:06:12 -0500

    change order of nav bar links

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -8,11 +8,11 @@ import { Link } from 'react-router-dom';
 const NavBar = () => {
   return (
     <div className='ui tabular menu'>
-      <div className="item"><Link to='/data' exact>Record Score</Link></div>
-      <div className="item"><Link to='/users/cards' exact>User Cards</Link></div>
-      <div className="item"><Link to='/users/highscores' exact>High Scores</Link></div>
       <div className="item"><Link to='/instructions' exact>Instructions</Link></div>
       <div className="item"><Link to='/animals' exact>All Animals</Link></div>
+      <div className="item"><Link to='/data' exact>Record Score</Link></div>
+      <div className="item"><Link to='/users/highscores' exact>High Scores</Link></div>
+      <div className="item"><Link to='/users/cards' exact>User Cards</Link></div>
     </div>
   );
 };
