80724ad5dfd48b9cd3acd69219abbbaa7c2c6291
Lizzie <lchan217@gmail.com>
2019-08-02 16:47:00 -0500

    change fetch requests to heroku api

--- src/actions/animalActions.js
+++ src/actions/animalActions.js
@@ -1,7 +1,7 @@
 export const fetchAnimals = () => {
   return (dispatch) => {
     dispatch({ type: 'LOADING_ANIMALS' });
-    return fetch('http://localhost:3001/api/animals')
+    return fetch('https://animals-game-api.herokuapp.com/api/animals')
       .then(response => response.json())
       .then(animals => dispatch({ type: 'FETCH_ANIMALS', payload: animals}));
   };
