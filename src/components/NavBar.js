b257d6425131b15ace440b10e2095e0dd3a84aef
Lizzie <lchan217@gmail.com>
2020-02-03 14:31:38 -0700

    convert to menu from semantic ui react and add formatting

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "semantic-ui-react";
+import "./css/NavBar.css";
 
 const NavBar = () => {
   return (
-    <div className="navBar">
-      <div className='ui tabular menu'>
-        <div className="item"><Link to='/'>Instructions</Link></div>
-        <div className="item"><Link to='/animals'>All Animals</Link></div>
-        <div className="item"><Link to='/data'>Record Score</Link></div>
-        <div className="item"><Link to='/users/highscores'>High Scores</Link></div>
-        <div className="item"><Link to='/users/cards'>User Cards</Link></div>
-      </div>
+    <div className='navBar'>
+      <Menu widths={5}>
+        <Menu.Item>
+          <Link to='/'>Instructions</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to='/animals'>All Animals</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to='/data'>Record Score</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to='/users/highscores'>High Scores</Link>
+        </Menu.Item>
+        <Menu.Item>
+          <Link to='/users/cards'>User Cards</Link>
+        </Menu.Item>
+      </Menu>
     </div>
   );
 };
