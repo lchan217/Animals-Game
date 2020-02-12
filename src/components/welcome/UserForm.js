8dc871b240d8664da3c606eeac64f2a81cf98426
Lizzie <lchan217@gmail.com>
2020-02-12 12:34:26 -0700

    add background to user-form

--- src/components/welcome/UserForm.js
+++ src/components/welcome/UserForm.js
@@ -34,80 +34,43 @@ class UserForm extends React.Component {
     const { name, age, occupation, score } = this.state;
     const { handleChange, handleSubmit } = this;
 
-    const background = {
-      width: "100%",
-      height: "800px",
-      padding: "40px",
-      backgroundImage: `url(https://images.unsplash.com/photo-1479046934034-119c613579a9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60)`,
-      backgroundSize: "cover"
-    };
     return (
-      <div style={background}>
+      <div className='score-form-background'>
         <Form className='score-form' onSubmit={handleSubmit}>
-          <Form.Field className='score-input'>
+          <Form.Input
+            className='score-input'
+            value={name}
+            onChange={handleChange}
+          >
             <label>Name</label>
             <input placeholder='Name' />
-          </Form.Field>
-          <Form.Field className='score-input'>
+          </Form.Input>
+          <Form.Input
+            className='score-input'
+            value={age}
+            onChange={handleChange}
+          >
             <label>Age</label>
             <input placeholder='Age' />
-          </Form.Field>
-          <Form.Field className='score-input'>
+          </Form.Input>
+          <Form.Input
+            className='score-input'
+            value={occupation}
+            onChange={handleChange}
+          >
             <label>Occupation</label>
             <input placeholder='Occupation' />
-          </Form.Field>
-          <Form.Field className='score-input'>
+          </Form.Input>
+          <Form.Input
+            className='score-input'
+            value={score}
+            onChange={handleChange}
+          >
             <label>Score</label>
             <input placeholder='Score' />
-          </Form.Field>
+          </Form.Input>
           <Button type='submit'>Submit</Button>
         </Form>
-
-        <form onSubmit={handleSubmit}>
-          <div className='field'>
-            <input
-              type='text'
-              name='name'
-              style={{ width: 400 }}
-              placeholder='Name'
-              value={name}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <input
-              type='text'
-              name='age'
-              style={{ width: 400 }}
-              placeholder='Age'
-              value={age}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <input
-              type='text'
-              name='occupation'
-              style={{ width: 400 }}
-              placeholder='Occupation'
-              value={occupation}
-              onChange={handleChange}
-            />
-          </div>
-          <div className='field'>
-            <input
-              type='text'
-              name='score'
-              style={{ width: 400 }}
-              placeholder='Score'
-              value={score}
-              onChange={handleChange}
-            />
-          </div>
-          <button className='ui submit button' type='submit'>
-            Submit
-          </button>
-        </form>
       </div>
     );
   }
