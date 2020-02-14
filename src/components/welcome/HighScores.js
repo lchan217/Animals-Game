4d623bef5c564712a75a3e3cc6b31150d8a37533
Lizzie <lchan217@gmail.com>
2020-02-14 09:13:02 -0700

    format fixed with prettier

--- src/components/welcome/HighScores.js
+++ src/components/welcome/HighScores.js
@@ -1,11 +1,12 @@
-import React from 'react';
-const HighScores = ({name, age, occupation, score, index}) =>
- {
-return (
-  <div className="highScores">
-    <p>Name: {name} | Score: {score} seconds </p>
-  </div>
-  )
-}
+import React from "react";
+const HighScores = ({ name, age, occupation, score, index }) => {
+  return (
+    <div className='high-scores'>
+      <p>
+        Name: {name} | Score: {score} seconds{" "}
+      </p>
+    </div>
+  );
+};
 
-export default HighScores
+export default HighScores;
