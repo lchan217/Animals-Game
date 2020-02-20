81b753698e2ab7030a758fcea3a394e90fa44f3d
Lizzie <lchan217@gmail.com>
2020-02-20 15:24:16 -0700

    move code from animalAction to goalAction

--- src/actions/animalActions.js
+++ src/actions/animalActions.js
@@ -7,15 +7,6 @@ export const fetchAnimals = () => {
   };
 };
 
-export const fetchGoals = () => {
-  return dispatch => {
-    dispatch({ type: "LOADING_GOALS" });
-    return fetch("https://animals-game-api.herokuapp.com/api/goals")
-      .then(response => response.json())
-      .then(goals => dispatch({ type: "FETCH_GOALS", payload: goals }));
-  };
-};
-
 export const nursingToWild = id => {
   return { type: "NURSING_TO_WILD", id };
 };
