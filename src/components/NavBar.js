c8dc6da77e6df72c78c164a83c32dc8d416179a7
Lizzie <lchan217@gmail.com>
2020-03-23 17:40:09 -0700

    remove play game from nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,13 +6,13 @@ import "../css/NavBar.css";
 const NavBar = () => {
   return (
     <div className='navBar'>
-      <Menu widths={3} fixed='true'>
+      <Menu widths={2} fixed='true'>
         <Menu.Item>
           <Link to='/'>Instructions</Link>
         </Menu.Item>
-        <Menu.Item>
+        {/* <Menu.Item>
           <Link to='/animals'>Play Game</Link>
-        </Menu.Item>
+        </Menu.Item> */}
         {/* <Menu.Item>
           <Link to='/data'>Record Score</Link>
         </Menu.Item> */}
