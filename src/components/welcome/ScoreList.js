a8dee230436ab54140ce254da25d1b168ab2586e
Lizzie <lchan217@gmail.com>
2020-03-26 09:47:39 -0700

    add moment

--- src/components/welcome/ScoreList.js
+++ src/components/welcome/ScoreList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Item } from "semantic-ui-react";
 import "../../css/ScoreList.css";
+import Moment from "react-moment";
 
 const ScoreList = ({ name, age, occupation, score, created_at }) => {
   return (
@@ -9,7 +10,9 @@ const ScoreList = ({ name, age, occupation, score, created_at }) => {
         <Item.Header>
           {name} | Score: {score} <br />
         </Item.Header>
-        <Item.Description>Date: {created_at}</Item.Description>
+        <Item.Description>
+          <Moment date={created_at} format='l' />
+        </Item.Description>
       </Item.Content>
     </Item>
   );
