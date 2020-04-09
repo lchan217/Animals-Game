4b3fd5caea55fc557b263b59a563cba8725c30e2
Lizzie <lchan217@gmail.com>
2020-04-09 10:36:22 -0700

    change fetch reqs to heroku

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,50 +1,52 @@
 export const fetchUsers = () => {
-  return dispatch => {
+  return (dispatch) => {
     dispatch({ type: "LOADING_USERS" });
     return (
-      // fetch("https://animals-game-api.herokuapp.com/api/users")
-      fetch("http://localhost:3001/api/users")
-        .then(response => response.json())
-        .then(users => dispatch({ type: "FETCH_USERS", payload: users }))
+      fetch("https://animals-game-api.herokuapp.com/api/users")
+        // fetch("http://localhost:3001/api/users")
+        .then((response) => response.json())
+        .then((users) => dispatch({ type: "FETCH_USERS", payload: users }))
     );
   };
 };
 
-export const addUser = user => {
-  return dispatch => {
+export const addUser = (user) => {
+  return (dispatch) => {
     const body = {
       name: user.name,
       age: user.age,
-      occupation: user.occupation
+      occupation: user.occupation,
     };
-    // fetch("https://animals-game-api.herokuapp.com/api/users", {
-    fetch("http://localhost:3001/api/users", {
+    fetch("https://animals-game-api.herokuapp.com/api/users", {
+      // fetch("http://localhost:3001/api/users", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     })
-      .then(resp => resp.json())
-      .then(user => dispatch({ type: "ADD_USER", user }));
+      .then((resp) => resp.json())
+      .then((user) => dispatch({ type: "ADD_USER", user }));
   };
 };
 
-export const addScore = score => {
-  return dispatch => {
+export const addScore = (score) => {
+  return (dispatch) => {
     const body = {
       time: score,
-      success: true
+      success: true,
     };
-    // fetch("https://animals-game-api.herokuapp.com/api/users", {
-    fetch("http://localhost:3001/api/users", {
+    fetch("https://animals-game-api.herokuapp.com/api/users", {
+      // fetch("http://localhost:3001/api/users", {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
-        Accept: "application/json"
+        Accept: "application/json",
       },
-      body: JSON.stringify(body)
-    }).then(resp => resp.json());
+      body: JSON.stringify(body),
+    })
+      .then((resp) => resp.json())
+      .then((user) => dispatch({ type: "ADD_SCORE", user }));
   };
 };
